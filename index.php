22b1b0188c4baf45cc3878980005850b437e47b4
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-08 14:36:47 +0100

    Arborecense

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php include("assets/includes/navbar.php") ?>  
+    <?php include("assets/includes/slider.php") ?>
+    <?php include("assets/includes/texte.php") ?>
+    <?php include("assets/includes/paralax.php") ?>
+    <?php include("assets/includes/arceus.php") ?>
+    <?php include("assets/includes/icon.php") ?>
+    <?php include("assets/includes/footer.php") ?>
+</body>
+</html>

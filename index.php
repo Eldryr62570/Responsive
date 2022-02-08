294b3eb5b3a7a9bf4a582044e4d2440d74ae3439
tintindrum <quentin-drumel@hotmail.com>
2022-02-08 16:31:55 +0100

    maj-slide

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300;600&family=Oswald:wght@200;400&family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("assets/includes/navbar.php") ?>
+    <?php include("assets/includes/slider.php") ?>
+    <?php include("assets/includes/texte.php") ?>
+    <?php include("assets/includes/paralax.php") ?>
+    <?php include("assets/includes/arceus.php") ?>
+    <?php include("assets/includes/icon.php") ?>
+    <?php include("assets/includes/footer.php") ?>
+
+    
+</body>
+</html>

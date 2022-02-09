58b960e70ef928ba5e4ee08f4115ed333da0bd90
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-09 20:28:34 +0100

    Update texte.php

--- assets/includes/texte.php
+++ assets/includes/texte.php
@@ -5,13 +5,7 @@
         Tout sur les guitares ! 
     </div>
     <div class="paragraphTexte">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo eligendi ab fuga iste, cum a porro, eaque accusamus id voluptas dolores aperiam quae eveniet suscipit, temporibus architecto reiciendis officia?
-        Autem earum accusamus iure! Reiciendis rem consequatur magni! Nam dolorum delectus nostrum! Obcaecati earum voluptatem vel, dolorum odit hic ad suscipit tenetur optio id molestiae fugit eius est non? Optio.
-        Delectus eum aut ea hic persp
-        iciatis libero recusandae deleniti harum voluptatem nobis enim rem nihil, laborum facilis? Omnis ratione possimus excepturi voluptate dolorem dignissimos, illum repellat. Ab enim laboriosam quo?
-        Recusandae, eius exercitationem. Enim quos quas nemo voluptate dolores alias, eveniet veniam, illo rem harum tempore omnis iusto illum sed nesciunt eos porro. Dolore, maiores? Ipsum, suscipit? Maxime, ad nobis!
-        Eum aliquid recusandae esse fugit at! Magnam sed hic et aspernatur! Dignissimos facere nobis quibusdam ut natus iusto, consequatur assumenda quae harum doloremque amet ea asperiores voluptate saepe veniam voluptatibus!
-        </p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         <div class="texteButton">
             <a href="#">En savoir plus ...</a>
         </div>

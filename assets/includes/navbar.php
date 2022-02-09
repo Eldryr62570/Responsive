3c9988645b65a09fef4648e3bfc4fa8dcccc5dea
Isooka <Andyh08440@gmail.com>
2022-02-09 10:09:10 +0100

    navbar + footer fini

---
+++ assets/includes/navbar.php
@@ -0,0 +1,27 @@
+<header>
+        
+        <nav>
+            <div class="logo">
+                <img src="assets/img/Logoblanc.png" width="50px" height="50px">
+            </div>
+            <ul class="nav-links">
+                <li>
+                    <a href="#acceuil"> Acceuil</a>
+                </li>
+                <li>
+                    <a href="#prestation"> Présentation</a>
+                </li>
+                <li>
+                    <a href="#actualité"> Histoire</a>
+                </li>
+                <li>
+                    <a href="#contact"> Contact</a>
+                </li>
+            </ul>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guitar'Hub</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300;600&family=Oswald:wght@200;400&family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    
+    <?php include("assets/includes/navbar.php") ?>
+    <?php include("assets/includes/slider.php") ?>
+    <?php include("assets/includes/texte.php") ?>
+    <?php include("assets/includes/paralax.php") ?>
+    <?php include("assets/includes/arceus.php") ?>
+    <?php include("assets/includes/icon.php") ?>
+    <?php include("assets/includes/footer.php") ?>
+
+    <script src="assets/js/nav.js"></script>
+    <script src="assets/js/backtotop.js"></script>
+
+</body>
+</html>

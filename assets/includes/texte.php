84b1f3c73c3dbb61b72fc2a96ea0f079694b16ce
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-09 09:10:07 +0100

    Fin parralax + texte avec bouton + responsive

--- assets/includes/texte.php
+++ assets/includes/texte.php
@@ -7,9 +7,14 @@
     <div class="paragraphTexte">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo eligendi ab fuga iste, cum a porro, eaque accusamus id voluptas dolores aperiam quae eveniet suscipit, temporibus architecto reiciendis officia?
         Autem earum accusamus iure! Reiciendis rem consequatur magni! Nam dolorum delectus nostrum! Obcaecati earum voluptatem vel, dolorum odit hic ad suscipit tenetur optio id molestiae fugit eius est non? Optio.
-        Delectus eum aut ea hic perspiciatis libero recusandae deleniti harum voluptatem nobis enim rem nihil, laborum facilis? Omnis ratione possimus excepturi voluptate dolorem dignissimos, illum repellat. Ab enim laboriosam quo?
+        Delectus eum aut ea hic persp
+        iciatis libero recusandae deleniti harum voluptatem nobis enim rem nihil, laborum facilis? Omnis ratione possimus excepturi voluptate dolorem dignissimos, illum repellat. Ab enim laboriosam quo?
         Recusandae, eius exercitationem. Enim quos quas nemo voluptate dolores alias, eveniet veniam, illo rem harum tempore omnis iusto illum sed nesciunt eos porro. Dolore, maiores? Ipsum, suscipit? Maxime, ad nobis!
         Eum aliquid recusandae esse fugit at! Magnam sed hic et aspernatur! Dignissimos facere nobis quibusdam ut natus iusto, consequatur assumenda quae harum doloremque amet ea asperiores voluptate saepe veniam voluptatibus!
         </p>
+        <div class="texteButton">
+            <a href="#">En savoir plus ...</a>
+        </div>
     </div>
+    
 </section>

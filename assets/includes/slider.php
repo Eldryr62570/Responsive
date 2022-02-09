dd54dc36abd38d4665e36c08a6f73deb5b30575d
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-09 16:01:51 +0100

    Button nightMode finiiiiiiiiii

--- assets/includes/slider.php
+++ assets/includes/slider.php
@@ -1,17 +1,4 @@
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
-    <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300;600&family=Oswald:wght@200;400&family=Roboto:wght@100;300&display=swap" rel="stylesheet">
-    <title></title>
-</head>
-<body>
-<!-- slide show + bouton -->
+
 <div class="desactiveimg"><img src="assets/img/guitareslide0.jpg" class="sizeImg"></div>
 <section class="slide-section">
     
@@ -27,8 +14,6 @@
             <i class="fas fa-chevron-circle-left"></i>
         </div>
     </div>
-    <div class="titre-slide"><h1>Les plus belles Guitares</h1></div>
+    <div class="titre-slide"><h1>Guitar' Hub</h1></div>
 </section>
     <script src="../../assets/js/slide.js"></script>
-</body>
-</html>
